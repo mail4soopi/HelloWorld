483ce86c8043c03a462b95916b3e1fe3e59b1fb8
vn000de <suresh.kumar.ramaswamy@cognizant.com>
2018-11-13 13:23:37 +0530

    testing Azure Hello World

--- src/test/scala/Test/MainTest.scala
+++ src/test/scala/Test/MainTest.scala
@@ -1,9 +1,12 @@
 package Test
 
+import org.scalatest.FlatSpec
 
 /**
   * Created by vn000de on 11/13/2018.
   */
-class MainTest extends {
-
+class MainTest extends FlatSpec {
+  "Application" should "add correctly" in {
+    assert(Application.add(1,2)==3)
+  }
 }
